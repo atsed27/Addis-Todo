6e47e1dc76a28c44440892019ecfb7394d8b24ca
atsed27 <danielnigatu09@gmail.com>
2024-02-04 14:27:06 -0300

    router is start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './page/Home';
+import Album from './page/Album';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn ReactD
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/album" element={<Album />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
